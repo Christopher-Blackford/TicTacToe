a7f4810df5aedb2dc67f5f6261e42ca7c3795d45
Christopher Blackford <christopher.blackford@mail.utoronto.ca>
2018-08-23 12:55:38 -0400

    Cleaned up an if statement
    Fixed if open_index doesn't exist. I.e. if neither computer nor player can win in one but there is also no place the computer can play where it has a free row/col/dia

--- TicTacToe.R
+++ TicTacToe.R
@@ -1,55 +1,61 @@
 ######################################################################
 ######################################################################
-###TicTacToe
+###Welcome to Tic-Tac-Toe in R
 
-#Clear workspace
-rm(list=ls())
-library(rgeos)
-library(sp)
+###Instructions:
+#To play, simply Run through each line below. 
+#You control the difficulty (line XX) and the moves you make on the board (lines , , , , )
+#Have fun!
+
+#Clear memory and get R ready to set up Tic-Tac-Toe
+rm(list=ls()); library(rgeos); library(sp)
 
 #Set-up game board, X and O graphics
 source("./code/Game board.R"); source("./code/Xs.R"); source("./code/Os.R")
 
-#########################################
-#Set difficulty! Options are "Easy", "Medium", "Hard", "Impossible"
-Difficulty <- "Easy"; source("./code/Setting difficulty.R") 
+#Set difficulty! You can set the difficulty to "Easy", "Medium", "Hard", and "Impossible"
+Difficulty <- "Impossible"; source("./code/Setting difficulty.R") 
 
+
+#Now you can begin playing. Player is "X", Computer is "O"
+#To identify where you want to go, set "Move_1" equal to a number from 1-9
+#Positions go from 1-9 across the board like you would read a book
 
 #########################################
 #Player Move 1
-Move_1 <- 6 #Positions go from 1-9 across the board like you would read a book
-source("./code/moves/Player/Player move 1.R")
-source("./code/moves/Computer/first turn/Computer move 1 V2.R") #Running player and computer move 1
+Move_1 <- 7
+source("./code/moves/Player/Player move 1.R") #Showing player's first move
+source("./code/moves/Computer/first turn/Computer move 1 V2.R") #Showing computer's first move
 
 
 #########################################
 #Player Move 2
-Move_2 <- 5
-source("./code/moves/Player/Player move 2.R")
-source("./code/moves/Computer/Computer move.R") #Running player and computer move 2
+Move_2 <- 6
+source("./code/moves/Player/Player move 2.R") #Showing player move
+source("./code/moves/Computer/Computer move.R") #Showing computer move
 
 
 #########################################
 #Player Move 3
-Move_3 <- 4
-source("./code/moves/Player/Player move 3.R")
-source("./code/moves/Computer/Computer move.R") #Running player and computer move 3
+Move_3 <- 8
+source("./code/moves/Player/Player move 3.R") #Showing player move
+source("./code/moves/Computer/Computer move.R") #Showing computer move
 
 
 #########################################
 #Player Move 4
 Move_4 <- 1
-source("./code/moves/Player/Player move 4.R")
-source("./code/moves/Computer/Computer move.R") #Running player, computer move 4 as well and fills in the last empty space
+source("./code/moves/Player/Player move 4.R") #Showing player move
+source("./code/moves/Computer/Computer move.R") #Showing computer move
+
 
 #########################################
 #Player Move 5
-source("./code/moves/Player/Player move 5 automatic.R")
+source("./code/moves/Player/Player move 5 automatic.R") #Only one space left to go, so this is automatic
 
 
+#Thanks for playing! To play again, go to the top of the page and re-run
 
-#####
 ####
-###
 ##
 #END
--- code/moves/Computer/Computer defence.R
+++ code/moves/Computer/Computer defence.R
@@ -82,9 +82,10 @@ if(win_index==3){mat[1,3]=10; plot(O_3, add=T)}}else{source("./code/moves/Comput
 #When neither player or computer are one turn away from winning and computer can afford to play aggressive
 }else if(mat[2,2] == 0){if(Difficulty_threshold < Difficulty_setting){mat[2,2]=10; plot(O_5, add=T)}else{source("./code/moves/Computer/Computer move suboptimally.R")}
 
-}else{if(Difficulty_threshold < Difficulty_setting){#Move_list <- c(Hor_1,Hor_2,Hor_3,Ver_1,Ver_2,Ver_3,Dia_1,Dia_2)
-open_space <- which(Move_list %in% 10) #Finding which rows, etc. are not blocked for the computer to play
-if(length(open_space) > 1){open_space <- sample(open_space,1)}else if(length(open_space) == 0){source("./code/moves/Computer/Computer move suboptimally.R")}
+}else{if(Difficulty_threshold < Difficulty_setting){
+  open_space <- which(Move_list %in% 10) #Finding which rows, etc. are not blocked for the computer to play
+  if(length(open_space) == 0){source("./code/moves/Computer/Computer move suboptimally.R")}
+  else if(length(open_space) >= 1){open_space <- sample(open_space,1)
 
 #########################################
 #########################################
@@ -134,7 +135,10 @@ if(open_index==3){mat[3,3]=10; plot(O_9, add=T)}
 }else if(open_space == 8){open_index <- sample(which(c(mat[3,1], mat[2,2], mat[1,3]) %in% 0),1)
 if(open_index==1){mat[3,1]=10; plot(O_7, add=T)}
 if(open_index==2){mat[2,2]=10; plot(O_5, add=T)}
-if(open_index==3){mat[1,3]=10; plot(O_3, add=T)}
-}
-}else{source("./code/moves/Computer/Computer move suboptimally.R")} #Computer plays randomly if it literally can find no other smart way to play
-}
+if(open_index==3){mat[1,3]=10; plot(O_3, add=T)}}
+  }#Closing if statements for if open_index exists
+  
+  }else{source("./code/moves/Computer/Computer move suboptimally.R")} #Computer plays randomly if it literally can find no other smart way to play
+
+  }#Closing part that starts 2nd paragraph "When neither player or computer are one turn away from winning and computer can afford to play aggressive"
+
